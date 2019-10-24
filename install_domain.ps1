b70bd6d5626dd1f2c3c0b77c41df02cf2d328aea
reswob10 <reswob10@gmail.com>
2019-10-23 20:18:40 -0400

    Add files via upload

---
+++ install_domain.ps1
@@ -0,0 +1,15 @@
+﻿
+#######  !!!!!!!!!!!! ATTENTION  !!!!!!!!!!!!!!!!!
+####
+#### Make sure you edit and replace all the < brackets and text between the brackets  > 
+#### 
+###### !!!!!!!!!!  ATTENTION  !!!!!!!!!!!!!!
+
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+ 
+$password = "<enter password here. Make it strong>" | ConvertTo-SecureString -AsPlainText -Force
+
+Install-ADDSForest -DomainName "<create domain name here. example: test.local>" -SafeModeAdministratorPassword $password -Force
+
